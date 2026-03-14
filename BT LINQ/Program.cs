6bdcdc85df5732b722a86391206293a31ff0f785
VOVANSANG06 <sangcute200611111-alt@gmail.com>
2026-03-14 16:16:05 +0700

    2415053122237 - Sang - Bai 18

--- BT LINQ/Program.cs
+++ BT LINQ/Program.cs
@@ -22,9 +22,12 @@ class Program
             new Student{Id=4, Name="Dung", Score=7}
         };
 
-        Console.WriteLine($"{info} | Bai 17");
-        double b17 = students.Average(s => s.Score);
-        Console.WriteLine("Diem trung binh SV: " + b17);
+        Console.WriteLine($"{info} | Bai 18");
+        var b18 = students.GroupBy(s => s.Score >= 8 ? "Gioi" : s.Score >= 6 ? "Kha" : "Trung binh");
+        foreach (var group in b18)
+        {
+            Console.WriteLine($"{group.Key}: {string.Join(", ", group.Select(s => s.Name))}");
+        }
 
         Console.ReadLine();
     }
